2de00e2036702726b5460cab9ebc0c27e6ea534f
chuchito2 <105559327+chuchito2@users.noreply.github.com>
2022-12-10 11:46:44 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Mi primer proyecto en la consola")
+var nombre = "Iker"
+var apellido = "García"
+var nombre_completo = nombre.concat(apellido);
+
+var a = 10;
+var b = 5;
+
+var suma = a + b;
+console.log(suma);
+
+var resta = a - b;
+console.log(resta);
+
+var multiplicar = a * b;
+console.log(multiplicar);
+
+var dividir = a / b;
+console.log(dividir);
